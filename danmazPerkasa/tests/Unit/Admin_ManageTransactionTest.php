3ab96b41b9907743a2c0043a197ffd1f4aa0510e
evftrya <viktaduwor29@gmail.com>
2025-07-02 03:44:54 +0700

    save

--- danmazPerkasa/tests/Unit/Admin_ManageTransactionTest.php
+++ danmazPerkasa/tests/Unit/Admin_ManageTransactionTest.php
@@ -13,17 +13,14 @@ class Admin_ManageTransactionTest extends TestCase
      * A basic unit test example.
      */
 
-    public function make_transaction_success(){
-        
-    }
     public function test_AcceptTransaction(): void{
-        $response = $this->get('/Transaction/AcceptOrder/10');
+        $response = $this->get('/Transaction/AcceptOrder/1');
         // dd($response->json());
         assertSame($response->json(),'Success');
     }
     
     public function test_RejectTransaction(): void{
-        $response = $this->get('/Transaction/RejectOrder/8');
+        $response = $this->get('/Transaction/RejectOrder/2');
         assertSame($response->json(),'Success');
     }
 }
